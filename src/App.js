abe84306eb831ad91cb59a87bbe86b8baf9f28d1
Kenneth Brook <kenneth.brook@hotmail.com>
2021-07-31 13:26:10 -0400

    animated header made

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import bg from'./assets/bg.jpg';
 function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(${bg})` }}>
-      <header className="App-header">
-        <h1>Where are the pancakes???</h1>
+      <header className="App-header neon-wraper">
+        <span className="txt">PANCAKECAT.COM</span>
+        <span className="grad"></span>
+        <span className="dodge"></span>
       </header>
     </div>
   );
