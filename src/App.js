414f7e0f04f5bdaa7a8ac2929d3c791bb56a4950
Kenneth Brook <kenneth.brook@hotmail.com>
2021-08-01 09:46:22 -0400

    build pack broke, will make new repo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React from 'react';
+import DisplayCard from './cards/DisplayCard';
+
 import bg from'./assets/bg.jpg';
 
 
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${bg})` }}>
-      <header className="App-header neon-wraper">
-        <span className="txt">PANCAKECAT.COM</span>
-        <span className="grad"></span>
-        <span className="dodge"></span>
-      </header>
+    <div>
+      <div className="App" style={{ backgroundImage: `url(${bg})` }}>
+        <header className="App-header neon-wraper">
+          <span className="txt">PANCAKECAT.COM</span>
+          <span className="grad"></span>
+          <span className="dodge"></span>
+        </header>
+      </div>
+      <div>
+      <DisplayCard />
+      </div>
     </div>
   );
 }
